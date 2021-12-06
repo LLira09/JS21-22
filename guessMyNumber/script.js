6c4bed29f12a40b1cd37b6ac7297e73ef69b1348
Jake Lira <llira09@gmail.com>
2021-12-06 15:08:21 -0600

    added basic game logic

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = document.querySelector('.guess').value;
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.highscore').textContent = score;
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start Guessing';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.highscore').textContent = 0;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = '';
+});
